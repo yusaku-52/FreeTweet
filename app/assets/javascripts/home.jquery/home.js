a40ed773417fb9d2749cba8560f24495f3174028
yusaku <yusaku0502@gmail.com>
2020-11-02 14:39:13 +0900

    スマホレスポンシブ

--- app/assets/javascripts/home.jquery/home.js
+++ app/assets/javascripts/home.jquery/home.js
@@ -1,19 +1,31 @@
-$(function () {
-    $('#slide-flash').slideDown('slow'); 
+document.addEventListener("turbolinks:load", function () {
+    $(function () {
+        $('#slide-flash').slideDown('slow'); 
 
-    $('.fa-times').click(function () {
-        $('#slide-flash').slideUp('slow');
-    });
+        $('.fa-times').click(function () {
+            $('#slide-flash').slideUp('slow');
+        });
 
-    $('.new-btn,.header-new-btn').click(function () {
-        $('.signup-modal').fadeIn(); 
-    });
+        $('.new-btn,.header-new-btn').click(function () {
+            $('.signup-modal').fadeIn(); 
+        });
 
-    $('.login-btn,.header-login-btn').click(function () {
-        $('.signin-modal').fadeIn();
-    });
+        $('.login-btn,.header-login-btn').click(function () {
+            $('.signin-modal').fadeIn();
+        });
 
-    $('.modal-remove').click(function () {
-        $('.signup-modal,.signin-modal').fadeOut(); 
+        $('.modal-remove').click(function () {
+            $('.signup-modal,.signin-modal').fadeOut(); 
+        });
+
+        //レスポンシブ
+
+        $('.menu-icon').click(function () {
+            $('.menu-list').animate({ 'marginRight': '205px' }, 500);
+        });
+
+        $('.menu-title').click(function () {
+            $('.menu-list').animate({ 'marginRight': '0px' }, 500);
+        });
     });
-});
+})
